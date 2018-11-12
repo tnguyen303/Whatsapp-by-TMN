88628209ac3bc5d9dec25c5ecb806a75de8671db
tnguyen303 <tringuyen552911@gmail.com>
2018-11-11 22:33:23 -0500

    added mongodb with heroku

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 //http package lets you run server without Express, socket.io requires more control that is provided with this package
-const server = require('http').createServer(app);
+const server = require("http").createServer(app);
 //package to use socket io
-const io = require('socket.io')(server);
-const mongoose = require('mongoose');
+const io = require("socket.io")(server);
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost/realTimeChat', {useNewUrlParser: true});
+mongoose.connect(
+//   "mongodb://localhost/realTimeChat",
+  'mongodb://user:password1@ds159563.mlab.com:59563/heroku_xk34dk23',
+  { useNewUrlParser: true }
+);
 
-require('./sockets/message-sockets')(io);
+require("./sockets/message-sockets")(io);
 
-require('./routes/html-routes')(app);
+require("./routes/html-routes")(app);
 
-require('./routes/api-routes')(app);
+require("./routes/api-routes")(app);
 
 server.listen(PORT, () => {
-    console.log('Server is listening at port', PORT);
-})
+  console.log("Server is listening at port", PORT);
+});
 
-const run = function(app){
-    console.log('run');
-}
+const run = function(app) {
+  console.log("run");
+};
